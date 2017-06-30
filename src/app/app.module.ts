043c086a19d3bc4ac2dd18cd431c979717442863
Surendra Bajracharya <Surendra.Bajracharya@wellsfargo.com>
2017-06-30 14:42:42 -0500

    Added 2 components member and product and used AngularRoute to navigate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { MemberComponent } from './member/member.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    MemberComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot([
+    {
+      path: 'member',
+      component: MemberComponent
+    },
+    {
+      path: 'product',
+      component: ProductComponent
+    }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
