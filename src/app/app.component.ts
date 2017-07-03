94fc474c97a09e610c0d3a860099e8d5d65d0073
Surendra Bajracharya <Surendra.Bajracharya@wellsfargo.com>
2017-07-03 14:44:25 -0500

    Base Converter demo with Event binding keyword is $event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,12 +20,22 @@ export class AppComponent {
   today = new Date(2017, 6, 30);
 
   form;
+  baseConverterForm;
+
+  hasBinaryChanged = 0;
+  hasHexaChanged = 0
 
   ngOnInit(){
       this.form = new FormGroup({
       firstname : new FormControl("Suru"),
       lastname : new FormControl(""),
       languages : new FormControl("")
+    });
+
+    this.baseConverterForm = new FormGroup({
+      decimal : new FormControl(""),
+      binary : new FormControl(""),
+      hexa : new FormControl("")
     });
   }
 
@@ -50,5 +60,42 @@ export class AppComponent {
     console.log(userdata);
   }
 
+//Base Converted demo -- 
+  decimalChanged = function (oldValue, newValue){
+    //console.log(newValue);
+    if(newValue != ""){
+      this.baseConverterForm.patchValue({binary:parseInt(newValue, 10).toString(2)});
+      this.baseConverterForm.patchValue({hexa:parseInt(newValue, 10).toString(16)});
+    }
+    else{
+      this.baseConverterForm.patchValue({binary:""});
+      this.baseConverterForm.patchValue({hexa:""});
+    }
+  }
 
+  binaryChanged = function( oldValue, newValue){
+    this.hasBinaryChanged += 1;
+    if(this.hasBinaryChanged == 1){
+      if(newValue != 1){
+        this.baseConverterForm.patchValue({decimal:parseInt(newValue, 2).toString(10)});
+      }
+      else{
+        this.baseConverterForm.patchValue({decimal:""});
+      }
+      this.hasBinaryChanged = 0;
+    }
+  }
+
+  hexaChanged = function( oldValue, newValue){
+    this.hasHexaChanged += 1;
+    if(this.hasHexaChanged == 1){
+      if(newValue != ""){
+        this.baseConverterForm.patchValue({decimal:parseInt(newValue, 16).toString(10)});
+      }
+      else{
+        this.baseConverterForm.patchValue({decimal:""});
+      }
+      this.hasHexaChanged = 0;
+    }
+  }
 }
